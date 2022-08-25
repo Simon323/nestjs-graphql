b91f828c5cb1a61988627a2765505f33dd728b80
Szymon Miękus <miekusoft@gmail.com>
2022-08-25 19:41:44 +0200

    Use configuration from .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        PORT: Joi.number().required(),
+      }),
+      envFilePath: './.env',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
