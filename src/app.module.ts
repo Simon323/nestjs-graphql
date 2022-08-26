90101b86b8ae3d268b6d064028a8d1ee77a436be
Szymon Miękus <miekusoft@gmail.com>
2022-08-26 20:57:39 +0200

    Cleanup src

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import * as Joi from 'joi';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 
 @Module({
   imports: [
@@ -14,7 +12,7 @@ import { AppService } from './app.service';
       envFilePath: './.env',
     }),
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
